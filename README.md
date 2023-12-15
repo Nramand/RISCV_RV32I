87830a6fd2b7b4bbba27ceb82dc6e96d20962e87
bemoaner <70214237+bemoaner@users.noreply.github.com>
2023-12-15 20:03:13 +0530

    riscv fetch rtl created with always keyword since iverilog not supporting always_ff

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # RISCV_RV32I
-RISCV 32 bit processor 
+RISCV 32 bit processor
+
+How to compile the rtl
+iverilog '-Wall' '-g2012' rsv_fetch.sv  &&  vvp a.out 
+
+
---
+++ rtl/rsv_fetch.sv
@@ -0,0 +1,45 @@
+
+module rsv_fetch (
+
+input  logic           clk,
+input  logic           reset_n,
+
+input  logic  [31:0]   pc_i, 
+
+// Output read request to Instruction Memory 
+output logic           fetch_mem_req_o,	
+output logic  [31:0]   fetch_mem_addr_o,
+
+//Read Data from the Memory 
+input  logic  [31:0]   mem_rd_inst_i,
+
+//Instruction output 
+output logic  [31:0]  fetch_mem_inst_o
+);
+
+   //--------------------------
+   // Internal signal
+   //-------------------------
+  logic fetch_mem_req_q;
+  
+  // Assert valid always since the processor will need a new
+  // instruction every cycle out from reset 
+  
+  always @(posedge clk or negedge reset_n)
+  begin 
+      if(!reset_n) begin 
+         fetch_mem_req_q <= 1'b0;
+      end
+      else begin 
+         fetch_mem_req_q <= 1'b1;
+      end
+  end
+  
+  
+  assign fetch_mem_req_o  = fetch_mem_req_q;
+  assign fetch_mem_addr_o = pc_i;
+  
+  // Memory read data contains the instruction to be processed 
+  assign fetch_mem_inst_o = mem_rd_inst_i;
+
+endmodule 
